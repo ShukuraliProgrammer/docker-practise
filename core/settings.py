74be52fda7567150ed8163d5d6c09f3ca0d5892f
shukurali <shukuralijob@gmail.com>
2025-01-25 22:53:28 +0500

    add domain to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
